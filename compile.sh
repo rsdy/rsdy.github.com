616ab6a549942ab7cb01e201cdb832b2cab046ca
rhapsodhy <me@rhapsodhy.hu>
2012-09-18 00:50:33 +0200

    [enh] added atom feed support. the code needs a bit of cleanup, but it generates a valid atom xml.

--- compile.sh
+++ compile.sh
@@ -1,18 +1,37 @@
 URL_BASE="http://rsdy.github.com"
+TIMESTAMP='%FT%TZ'
 
-alias pandoc="pandoc -f markdown -t html5 --template=template/template.html"
+alias pandoc_web="pandoc -f markdown -t html5 --template=template/template.html"
+alias pandoc_xml="pandoc -f markdown -t html5 --template=template/atom-item.xml"
+
+generate_article() {
+	markdown_link="[$(head -n1 $1 |cut -d' ' -f2-)]($URL_BASE/$target) ($3)"
+
+	sed -i "s/^% date/% $3/" $1
+	sed -i "/^-\+\$/ a\\ * $markdown_link" README.md
+
+	pandoc_web -V article -o "$2" "$1"
+	git add "$2" "$1"
+}
+
+generate_feed_entry() {
+	pandoc_xml -V root="$URL_BASE" -V filename="$3" -V timestamp="$4" -o "$2" "$1"
+	git add "$2" "$1"
+}
+
 for i in markdown/*.md; do
-	target="posts/$(basename "$i" .md)".html
+	mod_date="$(stat -c %y $i)"
+	bname="$(basename "$i" .md)"
+	html="posts/$bname.html"
+	feed="feed/$bname.xml"
 
-	[[ -f $target ]] || {
-		mod_date="$(stat -c %y $i |cut -d' '  -f1 |xargs date +'%d %B %Y' -d)"
-		markdown_link="[$(head -n1 $i |cut -d' ' -f2-)]($URL_BASE/$target) ($mod_date)"
-		sed -i "s/^% date/% $mod_date/" $i
-		sed -i "/^-\+\$/ a\\ * $markdown_link" README.md
-		pandoc -V article -o "$target" "$i"
-
-		git add "$target" "$i"
-	}
+	[[ -f $html ]] || generate_article "$i" "$html" "$(date +'%d %B %Y' -d"$mod_date")"
+	[[ -f $feed ]] || generate_feed_entry "$i" "$feed" "$html" "$(date +"$TIMESTAMP" -d"$mod_date")"
 done
-pandoc -o index.html README.md
-git add README.md index.html
+
+sed "s/\\\$timestamp\\\$/$(date -u +"$TIMESTAMP")/" template/atom.xml >atom.xml
+ls -t feed/ |sed 's;^;feed/;' |head -n10 |xargs cat >>atom.xml
+echo "</feed>" >>atom.xml
+
+pandoc_web -o index.html README.md
+git add README.md index.html atom.xml
